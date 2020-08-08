46986fd6de2ae4badba0f6e888e93466fb8ae7db
Gabriel Queiroz <gabrielqueirozzn@gmail.com>
2020-08-07 22:59:04 -0300

    adicionar jest-styled-components e ajustar testes

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from '@testing-library/react'
 import Main from '.'
 
-describe('<Main/>', () => {
+describe('<Main />', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    const { container } = render(<Main />)
+
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
-  it('should render colors concurrently', () => {
+
+  it('should render the colors correctly', () => {
     const { container } = render(<Main />)
 
     expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
